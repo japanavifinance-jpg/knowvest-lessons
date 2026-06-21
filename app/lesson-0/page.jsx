5c4186f0828380e71861463a70d124a1a34df8d1
Knowvest <japanavifinance@gmail.com>
2026-06-22 08:07:32 +0900

    update: deploy changes

--- app/lesson-0/page.jsx
+++ app/lesson-0/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
-//import { useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore, doc, setDoc, serverTimestamp } from "firebase/firestore";
 
